a9a298f07e3fc917f7bfb762419770176c8e200e
kira <agarwalaman020@gmail.com>
2019-05-07 15:18:39 +0530

    first

---
+++ precache-manifest.14bae0f819e30a9dd378faaf44335269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bf87fa36ecd238e14fa9614594929e",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "ddb11bbda67a21082abc",
+    "url": "/tictac/static/css/main.117296ce.chunk.css"
+  },
+  {
+    "revision": "d21f32997bea2a5a5569",
+    "url": "/tictac/static/js/2.aadb8e00.chunk.js"
+  },
+  {
+    "revision": "ddb11bbda67a21082abc",
+    "url": "/tictac/static/js/main.5025dd3c.chunk.js"
+  },
+  {
+    "revision": "d95a86ec2eb80a36c81e",
+    "url": "/tictac/static/js/runtime~main.dd4efcec.js"
+  }
+]);
